058334e89644828dbdd95083764ea682b57619c9
Hamza REMMAL <56235032+hamzaremmal@users.noreply.github.com>
2022-12-30 19:10:21 +0100

    Implement the FetchFiles phase

---
+++ src/main/scala/amyc/utils/FetchFiles.scala
@@ -0,0 +1,23 @@
+package amyc.utils
+
+import amyc.core.Context
+import amyc.reporter
+
+import java.io.File
+
+object FetchFiles extends Pipeline[List[String], List[File]]{
+
+  override val name = "FetchFiles"
+
+  override def run(paths: List[String])(using Context): List[File] =
+    val files = paths map(new File(_))
+
+    if (files.isEmpty) {
+      reporter.fatal("No input files")
+    }
+    files.find(!_.exists()).foreach { f =>
+      reporter.fatal(s"File not found: ${f.getName}")
+    }
+    files
+
+}
