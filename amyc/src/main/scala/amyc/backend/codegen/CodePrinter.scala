c2c575c321fa6130f68ce7bb8a87dfcf96c70297
Hamza REMMAL <56235032+hamzaremmal@users.noreply.github.com>
2022-12-31 23:31:03 +0100

    Separate .wasm file generation from the CodePrinter

--- amyc/src/main/scala/amyc/backend/codegen/CodePrinter.scala
+++ amyc/src/main/scala/amyc/backend/codegen/CodePrinter.scala
@@ -3,12 +3,11 @@ package amyc.backend.codegen
 import amyc.backend.wasm.{Module, wrapper}
 import amyc.utils.FileWriter
 import amyc.*
-import amyc.backend.wasm.wrapper.{HTMLWrapper, NodeJSWrapper, WATFile}
+import amyc.backend.wasm.wrapper.{HTMLWrapper, NodeJSWrapper, WASMFileGenerator, WATFile}
 import amyc.core.Context
 import amyc.utils.{Env, Pipeline}
 
-import scala.sys.process.*
-import java.io.*
+import java.io.File
 
 // Prints all 4 different files from a wasm Module
 object CodePrinter extends Pipeline[Module, Unit]{
@@ -19,55 +18,28 @@ object CodePrinter extends Pipeline[Module, Unit]{
 
   def nameWithExt(module: Module, ext: String) = s"${module.name}.$ext"
 
-  override val name = "CodePrinter"
-
-  override def run(m: Module)(using Context): Unit = {
-    val (local, inPath) = {
-      import Env._
-      os match {
-        case Linux   => ("./bin/wat2wasm",     "wat2wasm")
-        case Windows => ("./bin/wat2wasm.exe", "wat2wasm.exe")
-        case Mac     => ("./bin/wat2wasm",     "wat2wasm")
-      }
-    }
-
-    val w2wOptions = s"${pathWithExt(m, "wat")} -o ${pathWithExt(m, "wasm")}"
-
+  def mkOutputDirectory(using Context) =
     val outDir = new File(outDirName)
     if (!outDir.exists()) {
       outDir.mkdir()
     }
 
+  override val name = "CodePrinter"
+
+  override def run(m: Module)(using Context): Unit =
+    mkOutputDirectory
+    // Generate a *.wat file
     FileWriter(pathWithExt(m, "wat")){
       WATFile(m)
     }
-
-    try {
-      try {
-        s"$local $w2wOptions".!!
-      } catch {
-        case _: IOException =>
-          s"$inPath $w2wOptions".!!
-      }
-    } catch {
-      case _: IOException =>
-        reporter.fatal(
-          "wat2wasm utility was not found under ./bin or in system path, " +
-          "or did not have permission to execute. Make sure it is either in the system path, or in <root of the project>/bin"
-        )
-      case _: RuntimeException =>
-        reporter.fatal(s"wat2wasm failed to translate WebAssembly text file ${pathWithExt(m, "wat")} to binary")
-    }
-
+    // Generate a *.wasm file based on the *.wat file
+    WASMFileGenerator(m)
     // Web version needs path relative to .html
     FileWriter(pathWithExt(m, "html")){
       HTMLWrapper(nameWithExt(m, "wasm"), m)
     }
-
     // Node version needs path relative to project root
     FileWriter(pathWithExt(m, "js")){
       wrapper.NodeJSWrapper(pathWithExt(m, "wasm"), m)
     }
-
-  }
 }
---
+++ amyc/src/main/scala/amyc/backend/wasm/wrapper/WASMFileGenerator.scala
@@ -0,0 +1,45 @@
+package amyc.backend.wasm.wrapper
+
+import amyc.*
+import amyc.core.Context
+import amyc.backend.codegen.CodePrinter.pathWithExt
+import amyc.backend.wasm.Module
+import amyc.utils.Env
+
+import scala.sys.process.*
+import java.io.*
+
+object WASMFileGenerator {
+
+  def apply(m: Module)(using Context) = {
+    val (local, inPath) = {
+      import Env._
+      os match {
+        case Linux => ("./bin/wat2wasm", "wat2wasm")
+        case Windows => ("./bin/wat2wasm.exe", "wat2wasm.exe")
+        case Mac => ("./bin/wat2wasm", "wat2wasm")
+      }
+    }
+
+    val w2wOptions = s"${pathWithExt(m, "wat")} -o ${pathWithExt(m, "wasm")}"
+
+    try {
+      try {
+        s"$local $w2wOptions".!!
+      } catch {
+        case _: IOException =>
+          s"$inPath $w2wOptions".!!
+      }
+    } catch {
+      case _: IOException =>
+        reporter.fatal(
+          "wat2wasm utility was not found under ./bin or in system path, " +
+            "or did not have permission to execute. Make sure it is either in the system path, or in <root of the project>/bin"
+        )
+      case _: RuntimeException =>
+        reporter.fatal(s"wat2wasm failed to translate WebAssembly text file ${pathWithExt(m, "wat")} to binary")
+    }
+
+  }
+
+}
