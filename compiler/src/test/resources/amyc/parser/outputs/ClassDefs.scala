d74c19ee14d20d0c26eb6bfb21e0c0758fb105da
Hamza REMMAL <56235032+hamzaremmal@users.noreply.github.com>
2023-01-16 23:27:48 +0100

    Fix parser tests to drop `extends`

--- compiler/src/test/resources/amyc/parser/outputs/ClassDefs.scala
+++ compiler/src/test/resources/amyc/parser/outputs/ClassDefs.scala
@@ -1,7 +1,7 @@
 object CaseClassDefs
   abstract class Foo
-  case class Bar() extends Foo
-  case class Bar(v: Int) extends Foo
+  case class Bar() : Foo
+  case class Bar(v: Int) : Foo
   abstract class Foo
-  case class Bar(v: Int, v: A) extends Foo
+  case class Bar(v: Int, v: A) : Foo
 end CaseClassDefs
