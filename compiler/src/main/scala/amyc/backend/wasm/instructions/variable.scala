2f903f7751a5f0a3dd06eb8cba57ba2e908389ed
Hamza REMMAL <56235032+hamzaremmal@users.noreply.github.com>
2023-01-27 21:25:41 +0100

    Refactor code related to variable instructions

---
+++ compiler/src/main/scala/amyc/backend/wasm/instructions/variable.scala
@@ -0,0 +1,21 @@
+package amyc.backend.wasm.instructions
+
+import amyc.backend.wasm.indices.{globalidx, localidx}
+import amyc.backend.wasm.instructions.Instructions.Instruction
+
+/**
+  * https://webassembly.github.io/spec/core/text/instructions.html#variable-instructions
+  */
+object variable {
+
+  object global:
+    case class get(idx: globalidx) extends Instruction
+    case class set(idx: globalidx) extends Instruction
+
+  object local :
+
+    case class get(idx: localidx) extends Instruction
+    case class set(idx: localidx) extends Instruction
+    case class tee(idx: localidx) extends Instruction
+
+}
