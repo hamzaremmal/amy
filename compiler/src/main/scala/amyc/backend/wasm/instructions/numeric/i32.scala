59644aa5e5c3fae17109a1fcd2dd404da6e5e9d2
Hamza REMMAL <56235032+hamzaremmal@users.noreply.github.com>
2023-01-27 21:51:23 +0100

    add memory instructions to i32

--- compiler/src/main/scala/amyc/backend/wasm/instructions/numeric/i32.scala
+++ compiler/src/main/scala/amyc/backend/wasm/instructions/numeric/i32.scala
@@ -4,11 +4,15 @@ import amyc.backend.wasm.instructions.Instructions.Instruction
 
 /**
   * For Numeric Instructions : https://webassembly.github.io/spec/core/text/instructions.html#numeric-instructions
+  * For Memory Instructions : https://webassembly.github.io/spec/core/text/instructions.html#memory-instructions
   */
 object i32 :
 
-  case class const(value : Int) extends Instruction
+  // ==============================================================================================
+  // =================================== NUMERIC INSTRUCTIONS =====================================
+  // ==============================================================================================
 
+  case class const(value : Int) extends Instruction
   case object add extends Instruction
   case object sub extends Instruction
   case object mul extends Instruction
@@ -24,7 +28,6 @@ object i32 :
   case object shr_s extends Instruction
   case object rotl extends Instruction
   case object rotr extends Instruction
-
   case object eq extends Instruction
   case object eqz extends Instruction
   case object ne extends Instruction
@@ -36,29 +39,31 @@ object i32 :
   case object le_u extends Instruction
   case object lt_s extends Instruction
   case object lt_u extends Instruction
-
-
-
   case object clz extends Instruction
   case object ctz extends Instruction
   case object popcnt extends Instruction
-
   case object extend8_s extends Instruction
   case object extend16_s extends Instruction
-
   case object wrap_i64 extends Instruction
-
   case object trunc_f32_s extends Instruction
   case object trunc_f32_u extends Instruction
   case object trunc_f64_s extends Instruction
   case object trunc_f64_u extends Instruction
-
   case object trunc_sat_f32_s extends Instruction
-
   case object trunc_sat_f32_u extends Instruction
-
   case object trunc_sat_f64_s extends Instruction
-
   case object trunc_sat_f64_u extends Instruction
-
   case object reinterpret_f32 extends Instruction
+
+  // ==============================================================================================
+  // =================================== MEMORY INSTRUCTIONS ======================================
+  // ==============================================================================================
+
+  case object load extends Instruction
+  case object load8_s extends Instruction
+  case object load8_u extends Instruction
+  case object load16_s extends Instruction
+  case object load16_u extends Instruction
+  case object store extends Instruction
+  case object store8 extends Instruction
+  case object store16 extends Instruction
