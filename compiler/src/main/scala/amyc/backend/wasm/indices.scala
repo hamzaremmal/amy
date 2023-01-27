b86cb19ac00bf64dfcd25bcd98d187bc8f7296f8
Hamza REMMAL <56235032+hamzaremmal@users.noreply.github.com>
2023-01-27 19:27:38 +0100

    Add wasm indices

---
+++ compiler/src/main/scala/amyc/backend/wasm/indices.scala
@@ -0,0 +1,15 @@
+package amyc.backend.wasm
+
+object indices {
+
+  type typeidx = Int
+  type funcidx = Int
+  type tableidx = Int
+  type memidx = Int
+  type globalidx = Int
+  type elemidx = Int
+  type dataidx = Int
+  type localidx = Int
+  type labelidx = Int
+
+}
