06539315d1f4388d28f5ca549fa1a47f5f643b50
Hamza REMMAL <56235032+hamzaremmal@users.noreply.github.com>
2023-01-27 22:07:30 +0100

    complete control instructions

--- compiler/src/main/scala/amyc/backend/wasm/instructions/control.scala
+++ compiler/src/main/scala/amyc/backend/wasm/instructions/control.scala
@@ -1,12 +1,24 @@
 package amyc.backend.wasm.instructions
 
+import amyc.backend.wasm.indices.*
 import amyc.backend.wasm.instructions.Instructions.Instruction
 
-case object nop extends Instruction
-case object unreachable extends Instruction
-// Return
-case object ret extends Instruction
+/**
+  * https://webassembly.github.io/spec/core/text/instructions.html#control-instructions
+  */
 
+case object unreachable extends Instruction
+case object nop extends Instruction
+case class label(l: labelidx) extends Instruction
+case class br_if(l: labelidx) extends Instruction
+case class br_table(l: List[labelidx], ln: labelidx) extends Instruction
+case object `return` extends Instruction
+case class call(x: funcidx) extends Instruction
+
+// TODO HR : Add second parameter here after defining typeuse
+case class call_indirect(x: tableidx) extends Instruction
+
+// TODO HR : Should not be defined here
 case object end extends Instruction
 
 
