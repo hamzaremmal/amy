97d85cfd0eb520e00431751641afc4646bb0de4e
Hamza REMMAL <56235032+hamzaremmal@users.noreply.github.com>
2023-01-28 16:15:07 +0100

    Update LocalsHandler to be thread safe

--- compiler/src/main/scala/amyc/backend/wasm/utils/LocalsHandler.scala
+++ compiler/src/main/scala/amyc/backend/wasm/utils/LocalsHandler.scala
@@ -5,18 +5,19 @@ import amyc.core.Context
 import amyc.reporter
 
 import scala.collection.mutable
+import java.util.concurrent.atomic.AtomicInteger
 
 // TODO HR : Add support for id by changing Int to localidx
 // TODO HR : Add support for concurrency here
 final class LocalsHandler (val params : Int) :
 
   private val locals_ = mutable.HashMap.empty[Identifier, Int].withDefaultValue(-1)
-  private var locals_counter = params
+  private val locals_counter : AtomicInteger = AtomicInteger(params)
 
   def this(args : List[Identifier]) =
     this(args.size)
     // reset the counter to start create locals from 0
-    locals_counter = 0
+    locals_counter.set(0)
     // Register all the arguments in the
     for arg <- args do getFreshLocal(arg)
 
@@ -29,9 +30,7 @@ final class LocalsHandler (val params : Int) :
     locals_.getOrElseUpdate(id, getFreshLocal)
 
   def getFreshLocal : Int =
-    val t = locals_counter
-    locals_counter += 1
-    t
+    locals_counter.getAndIncrement()
 
   /* Number of defined locals in the scope of the handler */
-  def locals: Int = locals_counter - params
+  def locals: Int = locals_counter.get() - params
