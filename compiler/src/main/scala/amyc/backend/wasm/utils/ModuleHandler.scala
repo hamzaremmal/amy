10e89d061bcf4dd1d37249531cfd1a698f45ff31
Hamza Remmal <56235032+hamzaremmal@users.noreply.github.com>
2023-02-13 12:40:46 +0100

    Add memory management in ModuleHandler

--- compiler/src/main/scala/amyc/backend/wasm/utils/ModuleHandler.scala
+++ compiler/src/main/scala/amyc/backend/wasm/utils/ModuleHandler.scala
@@ -1,6 +1,7 @@
 package amyc.backend.wasm.utils
 
 import amyc.backend.wasm.*
+import amyc.backend.wasm.Instructions.{Code, Comment, global, id, local}
 import amyc.core.Context
 import amyc.core.Symbols.*
 import amyc.utils.UniqueCounter
@@ -8,19 +9,26 @@ import amyc.utils.UniqueCounter
 import java.util.concurrent.atomic.AtomicInteger
 import scala.collection.mutable
 
-class ModuleHandler(val name: String) :
+case class ModuleHandler(name: String) :
   
-  /* Atomic counter for constructors and functions */
+  /* Constructor indexes management */
   private val constrIndexes = new AtomicInteger
-  private val funIndexes = new AtomicInteger
-  
-  private val _freemem = new AtomicInteger
-  
   private val _constr: mutable.HashMap[Symbol, Int] = mutable.HashMap.empty
+
+  /* Function indexes management */
+  private val funIndexes = new AtomicInteger
   private val _fun: mutable.HashMap[Symbol, Int] = mutable.HashMap.empty
+
+  /* Memory management related */
+  private val _freemem = new AtomicInteger
+  private lazy val _freemem_id = id("free_mem")
 
   /* Pool of String literals to optimize strings in memory */
   private val _strpool: mutable.HashMap[String, Int] = mutable.HashMap.empty
+
+  // ==============================================================================================
+  // ========================================= API ================================================
+  // ==============================================================================================
   
   /* Compute the index of a constructor (id of a case class) */
   def constructor(sym: Symbol): Int =
@@ -29,30 +37,94 @@ class ModuleHandler(val name: String) :
   /* Compute the index of a function in the table */
   def function(sym: Symbol): Int =
     _fun.getOrElseUpdate(sym, funIndexes.getAndIncrement)
-    
+
+  /* Create the table with reference to all the registered functions */
   def table: Table =
-    val syms = _fun.toList.sorted((lhs, rhs) => lhs._2 - rhs._2).map(_._1.asInstanceOf[FunctionSymbol])
-    Table(syms)
+    Table{
+      for f <- _fun.toList.sorted((lhs, rhs) => lhs._2 - rhs._2)
+      yield
+        f._1.asInstanceOf[FunctionSymbol]
+    }
     
   // ==============================================================================================
   // ===================================== MEMORY MANAGEMENT ======================================
   // ==============================================================================================
-  
-  /* allocate memory */
-  def alloc(size: Int): Int = 
+
+  /**
+    * Allocate statically memory
+    * This is used for String literals for now
+    * @param size (Int) - size of the memory to allocate
+    * @return (Int) - base address of the allocated memory
+    */
+  def static_alloc(size: Int): Int =
     assert(size >= 0)
     _freemem.getAndAdd(size)
-  
-  /* allocating memory of size 0 will return the offset to the next free memory */
-  def boundary : Int = alloc(0)
-  
+
+  /**
+    * Base address of the next free memory
+    * @return (Int) - Base address of the next free memory
+    */
+  def static_boundary : Int =
+    // allocating memory of size 0 will return the offset to the next free memory
+    static_alloc(0)
+
+  /**
+    * Allocate dynamically memory.
+    *
+    * It leaves in the stack the base address for the object
+    * @param size (Int) - size to allocate
+    * @return (Code) - code to execute to allocate memory dynamically
+    *         leaves in the stack the base address (i32)
+    */
+  def dynamic_alloc(size: Int): Code =
+    withComment(s"dynamically allocating memory - size: $size"){
+      global.get(_freemem_id) <:>
+      // Computing the size of the object in memory
+      adtField(global.get(_freemem_id), size) <:>
+      // Storing the new size in memory
+      global.set(_freemem_id) <:>
+      Comment(s"end of dynamic allocation - size: $size")
+    }
+
+
+  /**
+    * Return dynamically the address of the next free memory
+    * @return (Code) - code to execute to get the memory boundary
+    *         leaves the address (i32) in the stack
+    */
+  def dynamic_boundary: Code =
+    global.get(_freemem_id)
+
+  /**
+    * Dynamically change the memory boundary
+    *
+    * THIS FUNCTION SHOULD BE REMOVED
+    * FOR NOW, IT'S SHOULD ONLY BE USED IN Std::readString
+    * @param code
+    * @return
+    */
+  def dynamic_boundary(code: Code): Code =
+    code <:>
+    global.set(_freemem_id)
+
   // ==============================================================================================
   // ===================================== STRING MANAGEMENT ======================================
   // ==============================================================================================
 
-  /* Find the reference in memory to the string or create a new one */
+  /**
+    * Fetch the address in memory of a String.
+    *
+    * If String is not in memory, it'll allocate memory
+    * @param str (String) - String to fetch
+    * @return (Int) - base address of the String
+    */
   def string(str: String) : Int =
-    _strpool.getOrElseUpdate(str, alloc(str.length + 1))
+    _strpool.getOrElseUpdate(str, static_alloc(str.length + 1))
 
+  /**
+    * Build the Data segments with the stored Strings in memory
+    * @return (List[Data]) - List of data segments
+    */
   def strpool: List[Data] =
-    _strpool.toList.map(s => Data(s._2, s._1))
+    for (str, address) <- _strpool.toList yield
+      Data(address, str)
