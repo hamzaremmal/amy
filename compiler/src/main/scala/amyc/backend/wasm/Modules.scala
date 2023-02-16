0acfc9ea4a52e1171597fc41872e967f3e44d58f
Hamza Remmal <56235032+hamzaremmal@users.noreply.github.com>
2023-02-16 18:53:24 +0100

    Redesign wasm modules file

--- compiler/src/main/scala/amyc/backend/wasm/Modules.scala
+++ compiler/src/main/scala/amyc/backend/wasm/Modules.scala
@@ -1,31 +1,35 @@
 package amyc.backend.wasm
 
-import amyc.utils.Preconditions.*
-import Instructions.{Code, i32}
+import amyc.*
+import amyc.ast.SymbolicTreeModule.{CaseClassDef, FunDef}
+import amyc.core.*
+import amyc.core.Symbols.*
+import amyc.backend.wasm.Indices.*
+import amyc.backend.wasm.Instructions.*
+import amyc.backend.wasm.Types.*
 import amyc.backend.wasm.Values.*
 import amyc.backend.wasm.utils.*
-import amyc.core.*
-import amyc.ast.SymbolicTreeModule.{CaseClassDef, FunDef}
-import amyc.backend.wasm.Types.*
-import amyc.backend.wasm.handlers.{LocalsHandler, ModuleHandler}
-import amyc.core.Symbols.*
-import amyc.symbols
-
-import scala.annotation.constructorOnly
+import amyc.backend.wasm.handlers.*
+import amyc.utils.Preconditions.*
 
 /**
-  * https://webassembly.github.io/spec/core/syntax/modules.html
+  * https://webassembly.github.io/spec/core/text/modules.html
   */
 object Modules :
-  // A WebAssembly module
-  case class Module(name: String,
-                    globals: List[Global],
-                    imports: List[String],
-                    table: Option[Table],
-                    data: List[Data],
-                    functions: List[Function])
 
-  // TODO HR : replace Function with F
+  /**
+    * A WebAssembly `import` statement
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#imports
+    */
+  case class Import(elem: String) // TODO HR : Doesn't follow the convention
+
+
+  /**
+    * A WebAssembly Function
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#functions
+    */
   case class Function private(name: id, params: List[param], result: Option[result], locals: List[local], code: Code, idx: Int)
 
   object Function {
@@ -73,10 +77,64 @@ object Modules :
 
   }
 
+  /**
+    * A WebAssembly Table
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#tables
+    */
   case class Table(elems: List[FunctionSymbol])
 
-  case class Data(offset: Int, str: String)
+  /**
+    * A WbeAssembly Memory
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#memories
+    */
+  case class Memory(id: Option[id]) // TODO HR: Still need to  add memtype
 
-  case class Global(value: Int)
+  /**
+    * A WebAssembly Global Variable
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#globals
+    */
+  case class Global(value: Int) // TODO HR : Doesn't follow the convention
 
-  case class Import(elem: String)
+  /**
+    * A WebAssembly `export` statement
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#exports
+    */
+  case object Export // TODO HR : Doesn't follow the convention
+
+  /**
+    * A WebAssembly Start Function
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#start-function
+    */
+  case class Start(idx: funcidx)
+
+  /**
+    * A WebAssembly element segment
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#element-segments
+    */
+  case object ElementSegment // TODO HR : Doesn't follow the convention
+
+  /**
+    * A WebAssembly data segment
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#data-segments
+    */
+  case class Data(offset: Int, str: String) // TODO HR : Doesn't follow the convention
+
+  /**
+    * A WebAssembly module
+    *
+    * https://webassembly.github.io/spec/core/text/modules.html#text-module
+    */
+  case class Module(name: String,
+                    globals: List[Global],
+                    imports: List[String],
+                    table: Option[Table],
+                    data: List[Data],
+                    functions: List[Function])
+  // TODO HR : Doesn't follow the convention
