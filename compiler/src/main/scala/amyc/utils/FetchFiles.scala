504812a477ab141003f07a723d760baf18def946
Hamza REMMAL <hamza@remmal.net>
2023-12-29 17:58:24 +0100

    Add the library to all the compilation runs

--- compiler/src/main/scala/amyc/utils/FetchFiles.scala
+++ compiler/src/main/scala/amyc/utils/FetchFiles.scala
@@ -5,20 +5,30 @@ import amyc.reporter
 
 import java.io.File
 
-object FetchFiles extends Pipeline[List[String], List[File]]{
+object FetchFiles extends Pipeline[List[String], List[File]]:
+
+  private val mandatory_files =
+    "./library/Boolean.amy" ::
+    "./library/Int.amy"     ::
+    "./library/List.amy"    ::
+    "./library/Option.amy"  ::
+    "./library/Std.amy"     ::
+    "./library/String.amy"  ::
+    "./library/Unit.amy"    ::
+    "./library/unnamed.amy" ::
+    Nil
 
   override val name = "FetchFiles"
 
   override def run(paths: List[String])(using Context): List[File] =
-    val files = paths map(new File(_))
-
-    if (files.isEmpty) {
+    if paths.isEmpty then
       reporter.fatal("No input files")
-    }
+
+    val files = (paths ++ mandatory_files).map(path => new File(path)).toSet
+
     files.find(!_.exists()).foreach { f =>
       reporter.fatal(s"File not found: ${f.getName}")
     }
-    reporter.info(s"Compiling ${files.length} Amy source file${if files.length == 1 then "" else "s"}")
-    files
 
-}
+    reporter.info(s"Compiling ${files.size} Amy source file${if files.size == 1 then "" else "s"}")
+    files.toList
